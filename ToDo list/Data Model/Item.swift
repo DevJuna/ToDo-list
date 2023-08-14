f0a2d07d8ad00ba2923cc9c0127a687a688bb2ce
DevJuna <junadeveloper@gmail.com>
2023-08-14 13:29:44 +0200

    Use of NSCoder to persist the data

---
+++ ToDo list/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  ToDo list
+//
+//  Created by Juleanny Navas on 14/08/2023.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
